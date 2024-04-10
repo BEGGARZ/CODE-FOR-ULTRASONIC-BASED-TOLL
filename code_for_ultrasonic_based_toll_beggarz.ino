fb72a833964104d81f608da03e70f6217bf0391b
BEGGARZ <166497931+BEGGARZ@users.noreply.github.com>
2024-04-10 08:53:48 +0545

    Add files via upload

---
+++ code_for_ultrasonic_based_toll_beggarz.ino
@@ -0,0 +1,82 @@
+
+
+#include <Servo.h>//includes the ibrary for servo 
+Servo sg;
+int trigPin = 6;
+int echoPin = 7;
+// defines variables
+long duration;
+int distance;
+
+
+
+void setup() 
+
+{
+
+  servo.attach(8);
+  servo.write(0);
+// Sets the trigPin as an Output
+pinMode(trigPin, OUTPUT);
+// Sets the echoPin as an Input 
+pinMode(echoPin, INPUT);
+
+}
+
+void loop() 
+
+{
+
+// Clears the trigPin
+
+digitalWrite(trigPin, LOW);
+
+delayMicroseconds(2);
+
+// Sets the trigPin on HIGH state for 10 micro seconds
+
+digitalWrite(trigPin, HIGH);
+
+delayMicroseconds(10);
+
+digitalWrite(trigPin, LOW);
+
+// Reads the echoPin, returns the sound wave travel time in microseconds
+
+duration = pulseIn(echoPin, HIGH);
+
+// Calculating the distance
+
+distance= duration*0.034/2;
+
+// Prints the distance on the Serial Monitor
+
+Serial.print("Distance: ");
+
+Serial.println(distance);
+
+if ( distance <= 25   ) // Change Distance according to Ultrasonic Sensor Placement
+
+ {
+
+
+
+servo.write(90);
+
+delay(3000);
+
+ } 
+
+else 
+
+{
+
+servo.write(0);
+
+
+
+ }
+
+
+
+}
